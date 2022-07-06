df136d719d993707dec21011caa49fdeabc336f8
EdaKlnc2000 <108608696+EdaKlnc2000@users.noreply.github.com>
2022-07-06 21:38:40 +0300

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main2 {
+        public static void main(String[] args) {
+            int r = 0;
+            double a,alan,pi = 3.14;
+            Scanner inp = new Scanner(System.in);
+            System.out.print("Dairenin alanını giriniz:");
+            alan = inp.nextDouble();
+             alan=Math.sqrt( pi * r * r);
+            pi=Math.sqrt((pi * (r*r) *(alan) / 360));
+            System.out.println("Dairenin alanı="+alan);
+
+
+
+
+        }
+    }
