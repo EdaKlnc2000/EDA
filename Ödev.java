40665edcc8048dcf00ac575d9698a4d33196fe8f
EdaKlnc2000 <108608696+EdaKlnc2000@users.noreply.github.com>
2022-07-07 19:04:55 +0300

    Add files via upload

---
+++ Ödev.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Ödev {
+    public static void main(String[]args){
+        int kilonuz=105;
+        double boyunuz=1.72;
+        Scanner inp=new Scanner(System.in);
+        System.out.print("Lütfen boyunuzu giriniz:");
+        boyunuz=inp.nextDouble();
+        System.out.print("Lütfen kilonuzu giriniz:");
+        kilonuz=inp.nextInt();
+        System.out.println("boyunuz=+boy");
+        System.out.println("kilonuz=+kilo");
+
+
+    }
+
+}
