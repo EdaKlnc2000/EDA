487319a0900670b3b82ab9e50238906fa5a8557e
EdaKlnc2000 <108608696+EdaKlnc2000@users.noreply.github.com>
2022-07-27 19:55:06 +0300

    Add files via upload

---
+++ Ödev5.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Ödev5 {
+    public static void main(String[] args) {
+        int heat;
+        Scanner inp = new  Scanner(System.in);
+        System.out.print("Sicaklilk Degerini Giriniz:");
+        heat = inp.nextInt();
+        switch (heat) {
+            case 5:{
+                System.out.println("Kayak Yapabilrisniz");
+                break;
+
+            }
+            case 25:{
+                System.out.println("Simemaya gidebilirisiniz");
+                break;
+            }
+            case 15:{
+                System.out.println("Simemaya gidebilirisiniz");
+                break;
+            }
+            case 10:{
+                System.out.println("Pikniğe gidebilirsiniz");
+                break;
+            }
+            default:{
+                System.out.println("Yüzmeye gidebilirsiniz");
+            }
+
+
+
+
+
+
+                }
+            }
+        }
+
+
+
+
+
+
