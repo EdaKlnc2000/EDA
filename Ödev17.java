6d29d9986914c8490fc86a0dafd80c4b3c318b60
EdaKlnc2000 <108608696+EdaKlnc2000@users.noreply.github.com>
2022-08-21 10:39:31 +0300

    Add files via upload

---
+++ Ödev17.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Ödev17 {
+    public static void main(String[]args){
+        Scanner inp=new Scanner(System.in);
+        int n;
+        double result=0;
+        System.out.print("N Sayisini Giriniz:");
+        n=inp.nextInt();
+        for(int i=1; i<=n;i++){
+            result+=(1/1);
+        }System.out.println(result);
+    }
+}
