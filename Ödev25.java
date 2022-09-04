05b52095783e9f89fdca3a58af68023a9318154e
EdaKlnc2000 <108608696+EdaKlnc2000@users.noreply.github.com>
2022-09-04 08:50:52 +0300

    Add files via upload

---
+++ Ödev25.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Ödev25 {
+    public static void main(String[]args){
+      int N;
+      Scanner inp=new Scanner(System.in);
+      System.out.print(" Sayi Giriniz:");
+      N=inp.nextInt();
+      for(int i=1;i<=10;i++){
+          System.out.println(N+" * "+i+" = "+N*i);
+      }
+
+
+      }
+
+
+
+          }
+
+
+
