58d6990cba4adf75161683610a4eea5a592bef8b
EdaKlnc2000 <108608696+EdaKlnc2000@users.noreply.github.com>
2023-01-22 15:31:11 +0300

    Add files via upload

--- Eda.java
+++ Eda.java
@@ -1,14 +1,20 @@
-import java.util.Scanner;
 public class Eda {
-    public static void main(String[]args) {
-        Scanner inp=new Scanner(System.in);
-        int km;
-        double perkm=2.20,total;
-        System.out.println("Taksimetre km cinsinden giriniz:");
-        km=inp.nextInt();
-         total=+(km*perkm);
-        total=(total<20)?20:total;
-        System.out.println("toplam tutar:"+total);
+    public static void recursiveMethod(int num) {
+        if (num <= 0) {
+            num += 5;
+            System.out.println("New value: " + num);
+            recursiveMethod(num);
+        } else {
+            num -= 5;
+            System.out.println("New value: " + num);
+            recursiveMethod(num);
+        }
+            int num = 25;
+            recursiveMethod(num);
 
+        }
     }
-}
+
+
+
+
