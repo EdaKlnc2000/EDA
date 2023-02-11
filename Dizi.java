01bd7fa82bba861e3348da135c04208dc5af70c3
EdaKlnc2000 <108608696+EdaKlnc2000@users.noreply.github.com>
2023-02-11 15:45:47 +0300

    Add files via upload

---
+++ Dizi.java
@@ -0,0 +1,17 @@
+public class Dizi {
+    public static void main(String[]args){
+        int[]list={5,10,20,30,40,50};
+        int[]Eda={10,20,30,40,50};
+        System.out.println("Örnek Giriş:");
+        for(int a=0;a<list.length;a++){
+            System.out.println(list[a]);
+        }
+        System.out.println("------------");
+        System.out.println("Örnek Çıktı:");
+        for(int i=0;i<Eda.length;i++){
+            System.out.println(Eda[i]);
+        }
+
+    }
+
+}
